b9b74736890442a46d40522dd91f274926ce8999
raiprads <raiprads@gmail.com>
2016-07-24 23:42:52 +0300

    File updates end of V07

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,7 @@
 Route::get('/','PagesController@home');
 
 Route::get('about','PagesController@about');
+
+Route::get('cards','CardsController@index');
+
+Route::get('cards/{card}','CardsController@show');
